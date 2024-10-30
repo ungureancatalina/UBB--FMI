5e9aa4f424fe38c5b5d46cc3fca7e4dee8b86249
Ungurean Cătălina <149062643+ungureancatalina@users.noreply.github.com>
2024-10-30 10:39:22 +0200

    Create a_1.asm

---
+++ AN_1/SEM_1/ASC/exersare practic/1/a_1.asm
@@ -0,0 +1,53 @@
+;Sa se citeasca de la tastatura doua numere (in baza 10) si sa se calculeze produsul lor. Rezultatul inmultirii se va salva in memorie in variabila "rezultat" (definita in segmentul de date).
+
+bits 32
+global start
+extern exit, printf, scanf
+import exit msvcrt.dll
+import printf msvcrt.dll
+import scanf msvcrt.dll
+
+segment data use32 class=data
+
+a resd 1
+b resd 1
+rezultat resq 1
+mesaj1 db "Numarul a este: ", 0
+mesaj2 db "Numarul b este: ", 0
+format db "%d", 0
+format2 db "Rezultat este: %d", 0
+
+segment code use32 class=code
+
+start:
+    push dword mesaj1
+    call [printf]
+    add esp,4*1
+
+    push dword a
+    push dword format
+    call [scanf]
+    add esp,4*2
+    
+    push dword mesaj2
+    call [printf]
+    add esp,4*1
+
+    push dword b
+    push dword format
+    call [scanf]
+    add esp,4*2
+    
+    mov eax,[a]
+    imul dword [b]
+    mov dword [rezultat],eax
+    mov dword [rezultat+4],edx
+    
+    push dword [rezultat+4]
+    push dword [rezultat]
+    push dword format2
+    call [printf]
+    add esp,4*3
+    
+push dword 0
+call [exit]
