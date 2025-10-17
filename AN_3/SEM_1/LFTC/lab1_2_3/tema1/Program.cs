57c51a9b3d464447e608ba993f1803c6cc25e5a2
Ungurean Cătălina <149062643+ungureancatalina@users.noreply.github.com>
2025-10-17 10:59:29 +0300

    .

---
+++ AN_3/SEM_1/LFTC/lab1_2_3/tema1/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace tema1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+public class Program
+{ 
+    // Coduri lexeme
+    static Dictionary<string, int> coduri = new Dictionary<string, int>()
+    {
+        {"ID",0},{"CONST_NUM",1},{"CONST_SIR",2},
+        {"int",3},{"double",4},{"siruri",5},
+        {"daca",6},{"altfel_daca",7},{"altfel",8},
+        {"atata_timp_cat",9},{"continua_pentru",10},
+        {"citire",11},{"afisare",12},
+        {"(",13},{")",14},{"{",15},{"}",16},
+        {";",17},{",",18},{"=",19},{"+",20},{"-",21},
+        {"*",22},{"/",23},{"%",24},{"==",25},{"!=",26},
+        {"<",27},{">",28},{"<=",29},{">=",30},
+        {"start",31},{"end",32}
+    };
+
+    static HashSet<string> cuvinteCheie = new HashSet<string>()
+    {
+        "int","double","siruri","daca","altfel_daca","altfel",
+        "atata_timp_cat","continua_pentru","citire","afisare","start","end"
+    };
+
+    static HashSet<string> operatoriSeparatoare = new HashSet<string>()
+    {
+        "(",")","{","}",";",",","=","+","-","*","/","%",
+        "==","!=","<",">","<=",">="
+    };
+
+    // Regex pentru ID și constante
+    static Regex regexID = new Regex(@"^[0-9][0-9A-Za-z_]{0,199}$");
+    static Regex regexConstNum = new Regex(@"^[+-]?[0-9]+(\.[0-9]+)?$");
+    static Regex regexConstSir = new Regex("^\".*\"$");
+
+    // Tabele TS si FIP
+    static List<string> TS = new List<string>();
+    static List<(int cod, int tsIndex)> FIP = new List<(int, int)>();
+
+    static int AdaugaInTS(string lexem)
+    {
+        if (TS.Contains(lexem))
+            return TS.IndexOf(lexem) + 1;
+        TS.Add(lexem);
+        return TS.Count;
+    }
+
+    static void Main()
+    {
+        string text = File.ReadAllText("program.txt");
+
+        // Eliminam comentariile
+        text = Regex.Replace(text, @"//.*", "");
+        text = text.Replace("\n", " \n "); // pastram newline pentru tokenizare
+
+        // Tokenizare simpla
+        string pattern = @"(\s+|==|!=|<=|>=|[\+\-\*/%(),;{}])";
+        string[] tokens = Regex.Split(text, pattern);
+        List<string> tokensCurate = new List<string>();
+        foreach (string t in tokens)
+        {
+            if (!string.IsNullOrWhiteSpace(t))
+                tokensCurate.Add(t);
+        }
+
+        // Recunoastere lexeme
+        foreach (string tok in tokensCurate)
+        {
+            if (cuvinteCheie.Contains(tok))
+            {
+                FIP.Add((coduri[tok], 0));
+            }
+            else if (operatoriSeparatoare.Contains(tok))
+            {
+                FIP.Add((coduri[tok], 0));
+            }
+            else if (regexID.IsMatch(tok))
+            {
+                int idx = AdaugaInTS(tok);
+                FIP.Add((coduri["ID"], idx));
+            }
+            else if (regexConstNum.IsMatch(tok))
+            {
+                int idx = AdaugaInTS(tok);
+                FIP.Add((coduri["CONST_NUM"], idx));
+            }
+            else if (regexConstSir.IsMatch(tok))
+            {
+                int idx = AdaugaInTS(tok);
+                FIP.Add((coduri["CONST_SIR"], idx));
+            }
+            else if (tok == "\n")
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine($"Eroare lexicala: '{tok}'");
+            }
+        }
+
+        // Scriere TS
+        using (StreamWriter sw = new StreamWriter("ts.txt"))
+        {
+            sw.WriteLine("Idx\tLexem");
+            for (int i = 0; i < TS.Count; i++)
+            {
+                sw.WriteLine($"{i + 1}\t{TS[i]}");
+            }
+        }
+
+        // Scriere FIP
+        using (StreamWriter sw = new StreamWriter("fip.txt"))
+        {
+            sw.WriteLine("Cod\tTS_idx");
+            foreach (var (cod, idx) in FIP)
+            {
+                sw.WriteLine($"{cod}\t{idx}");
+            }
+        }
+    }
+}
---
+++ AN_3/SEM_1/LFTC/lab1_4/tema1_4/Program.cs
@@ -0,0 +1,169 @@
+﻿// cata - analizor lexical cu verificare start/end
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Tema1
+{
+    public class Program
+    {
+        // dictionar cu codurile pentru fiecare atom lexical (tabel de coduri)
+        static readonly Dictionary<string, int> coduri = new Dictionary<string, int>()
+        {
+            {"ID",0},{"CONST_NUM",1},{"CONST_SIR",2},
+            {"int",3},{"double",4},{"siruri",5},
+            {"daca",6},{"altfel_daca",7},{"altfel",8},
+            {"atata_timp_cat",9},{"continua_pentru",10},
+            {"citire",11},{"afisare",12},
+            {"(",13},{")",14},{"{",15},{"}",16},
+            {";",17},{",",18},{"=",19},{"+",20},{"-",21},
+            {"*",22},{"/",23},{"%",24},{"==",25},{"!=",26},
+            {"<",27},{">",28},{"<=",29},{">=",30},
+            {"start",31},{"end",32}
+        };
+
+        // multime cu toate cuvintele cheie
+        static readonly HashSet<string> cuvinteCheie = new HashSet<string>()
+        {
+            "int","double","siruri","daca","altfel_daca","altfel",
+            "atata_timp_cat","continua_pentru","citire","afisare","start","end"
+        };
+
+        // multime cu toti operatorii si separatorii
+        static readonly HashSet<string> operatoriSeparatoare = new HashSet<string>()
+        {
+            "(",")","{","}",";",",","=","+","-","*","/","%",
+            "==","!=","<",">","<=",">="
+        };
+
+        // expresii regulate pentru recunoasterea identificatorilor si constantelor
+        static readonly Regex regexID = new Regex(@"^[0-9][0-9A-Za-z_]{0,199}$");
+        static readonly Regex regexConstNum = new Regex(@"^[+-]?[0-9]+(\.[0-9]+)?$");
+        static readonly Regex regexConstSir = new Regex("^\".*\"$");
+
+        // tabela de simboluri (TS) - ordonata lexicografic
+        static SortedSet<string> TS = new SortedSet<string>();
+
+        // dictionar pentru acces rapid Θ(1) la pozitia din TS
+        static Dictionary<string, int> TSIndex = new Dictionary<string, int>();
+
+        // forma interna a programului (FIP)
+        static List<(int cod, int tsIndex)> FIP = new List<(int, int)>();
+
+        // contorizam aparitiile lui start si end pentru verificare
+        static int countStartEnd = 0;
+
+        // functie care adauga un lexem in tabela de simboluri
+        static int AdaugaInTS(string lexem)
+        {
+            if (TSIndex.ContainsKey(lexem))
+                return TSIndex[lexem];
+
+            TS.Add(lexem);
+
+            var sortedList = new List<string>(TS);
+            TSIndex.Clear();
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                TSIndex[sortedList[i]] = i + 1;
+            }
+
+            return TSIndex[lexem];
+        }
+
+        public static void Main()
+        {
+            string inputPath = "program.txt";
+            if (!File.Exists(inputPath))
+            {
+                Console.WriteLine("Eroare: fisierul program.txt nu exista!");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(inputPath);
+            int lineNumber = 0;
+
+            foreach (string line in lines)
+            {
+                lineNumber++;
+
+                // eliminam comentariile
+                string text = Regex.Replace(line, @"//.*", "");
+                text = text.Replace("\t", " ");
+
+                // expresie pentru separarea tokenilor
+                string pattern = @"(\s+|==|!=|<=|>=|[\+\-\*/%(),;{}=])";
+                string[] tokens = Regex.Split(text, pattern);
+
+                foreach (string tok in tokens)
+                {
+                    string t = tok.Trim();
+                    if (string.IsNullOrEmpty(t)) continue;
+
+                    if (cuvinteCheie.Contains(t))
+                    {
+                        FIP.Add((coduri[t], 0));
+
+                        // contorizam aparitiile de start si end
+                        if (t == "start" || t =="end") countStartEnd++;
+                    }
+                    else if (operatoriSeparatoare.Contains(t))
+                    {
+                        FIP.Add((coduri[t], 0));
+                    }
+                    else if (regexID.IsMatch(t))
+                    {
+                        int idx = AdaugaInTS(t);
+                        FIP.Add((coduri["ID"], idx));
+                    }
+                    else if (regexConstNum.IsMatch(t))
+                    {
+                        int idx = AdaugaInTS(t);
+                        FIP.Add((coduri["CONST_NUM"], idx));
+                    }
+                    else if (regexConstSir.IsMatch(t))
+                    {
+                        int idx = AdaugaInTS(t);
+                        FIP.Add((coduri["CONST_SIR"], idx));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Eroare lexica la linia {lineNumber}: token necunoscut '{t}'");
+                    }
+                }
+            }
+
+            // verificare existenta start si end
+            if (countStartEnd == 0)
+            {
+                Console.WriteLine("Eroare: lipsesc 'start' sau/si 'end' in program.");
+            }
+
+            // salvam tabela de simboluri (TS)
+            var sortedTS = new List<string>(TS);
+            using (StreamWriter sw = new StreamWriter("ts.txt"))
+            {
+                sw.WriteLine("Idx\tLexem");
+                for (int i = 0; i < sortedTS.Count; i++)
+                {
+                    sw.WriteLine($"{i + 1}\t{sortedTS[i]}");
+                }
+            }
+
+            // salvam forma interna a programului (FIP)
+            using (StreamWriter sw = new StreamWriter("fip.txt"))
+            {
+                sw.WriteLine("Cod\tTS_idx");
+                foreach (var (cod, idx) in FIP)
+                {
+                    sw.WriteLine($"{cod}\t{idx}");
+                }
+            }
+
+            Console.WriteLine("Analiza lexica finalizata.");
+            Console.WriteLine($"TS: {sortedTS.Count} simboluri | FIP: {FIP.Count} intrari");
+        }
+    }
+}
